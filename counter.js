61c8f1e4702e3f6bcff5475417629fa32b1e074c
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2023-12-21 20:41:01 +0900

    Initial commit

---
+++ counter.js
@@ -0,0 +1,33 @@
+import yorkie from 'yorkie-js-sdk';
+
+const client = new yorkie.Client('https://api.yorkie.dev', {
+  apiKey: '',
+});
+await client.activate();
+
+const doc = new yorkie.Document('counter');
+await client.attach(doc);
+
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => {
+    doc.update((root)=>{
+      root.counter.increase(1)
+    })
+  })
+
+  doc.update((root) =>{
+    if(root.counter){
+      setCounter(root.counter.getValue())
+    }else{
+      root.counter = new yorkie.Counter(yorkie.IntType, counter)
+    }
+  })
+  doc.subscribe((event)=>{
+    setCounter(doc.getRoot().counter.getValue())
+  })
+}
